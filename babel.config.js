821c88a12118e628c6792da92454069d07ca08bb
Luiza Prata Soldi Passos <luizapassos@Luizas-MacBook-Pro.local>
2020-08-05 21:34:28 -0300

    ajusta babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
 };
