7b21931f6fb9c2366c865a1b581a5a0ad1acfb22
Luiza Prata Soldi Passos <luizapassos@Luizas-MacBook-Pro.local>
2020-08-06 18:34:39 -0300

    ajusta texto testes

--- src/calculadora/Calculadora.unit.spec.js
+++ src/calculadora/Calculadora.unit.spec.js
@@ -1,6 +1,5 @@
-import Calculadora from './Calculadora.js';
-import { logger } from '../infraestrutura/logger.js';
+test('Dado a lista [1,2,3] quando realizar a soma então deve retornar 6', () => {});
+test('Dado a lista [4,5] quando realizar a soma então deve acrescentar 9 à variável total', () => {});
+test('Dado a lista [5,3,7] quando realizar a soma então deve chamar o info do logger com valor "total atualizado: 15"', () => {});
 
-test('Dado a soma da lista [1,2,3] deve retornar 6', () => {});
-test('Dado a soma da lista [1,2,3] deve acrescentar 6 à variável total', () => {});
-test('Dado a soma da lista c[4,5,6] deve chamar o info do logger com valor "total atualizado: 15"', () => {});
+//BDD - DADO um cenário QUANDO ocorrer uma ação DEVE ocorrer uma saída
