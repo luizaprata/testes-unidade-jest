801981670815bdd35c514f518093557e131e263f
Luiza Prata Soldi Passos <luizapassos@Luizas-MacBook-Pro.local>
2020-08-06 18:28:17 -0300

    ajusta caminho pastas

---
+++ src/calculadora/Calculadora.unit.spec.js
@@ -0,0 +1,6 @@
+import Calculadora from './Calculadora.js';
+import { logger } from '../infraestrutura/logger.js';
+
+test('Dado a soma da lista [1,2,3] deve retornar 6', () => {});
+test('Dado a soma da lista [1,2,3] deve acrescentar 6 à variável total', () => {});
+test('Dado a soma da lista c[4,5,6] deve chamar o info do logger com valor "total atualizado: 15"', () => {});
--- src/parquinho/Calculadora.unit.spec.js
+++
@@ -1,5 +0,0 @@
-import Calculadora from './Calculadora.js';
-
-test('Dado a soma da lista [1,2,3] deve retornar 6', () => {});
-test('Dado a soma de [1,2,3] deve acrescentar 6 à variável total', () => {});
-test('Dado a soma [4,5,6] deve chamar o info do logger com valor "total atualizado: 15"', () => {});
